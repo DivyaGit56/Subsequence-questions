6dcf2062da60093cf83437f00eddc842d3df853a
Divya singh <diyasingh2056@gmail.com>
2026-09-11 19:57:00 +0530

    Update Subsequence with k sum.java

--- Subsequence with k sum.java
+++ Subsequence with k sum.java
@@ -21,3 +21,35 @@ class Solution {
       return  solve(arr,0,sum,k);
     }
 }
+
+// With Memoization
+
+class Solution {
+   Boolean dp[][];
+    public boolean solve(int []arr,int i, int sum, int k){
+        if(sum == k){
+            return true;
+        }
+        if(i == arr.length || sum > k){
+            return false;
+        }
+        
+        if (dp[i][sum] != null) {
+                    return dp[i][sum];
+                }
+
+        // add curr index to sum
+       boolean take =  solve(arr,i+1,sum+arr[i],k);
+        
+        // skip
+        boolean skip = solve(arr,i+1,sum,k);
+        
+        return dp[i][sum] = take || skip;
+    }
+    public boolean checkSubsequenceSum(int[] arr, int k) {
+        // code here
+        
+        dp = new Boolean[arr.length][k+1];
+      return  solve(arr,0,0,k);
+    }
+}
